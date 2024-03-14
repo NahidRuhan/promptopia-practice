bfd7254a5ab59619aeec3be2a396548ed08a79fe
NahidRuhan <dilal328@gmail.com>
2024-03-14 18:46:19 +0600

    commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,28 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover and Share AI Prompt"
-}
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
-  return (
-    <html lang="en">
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-        <body>
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
-            <div className='main'>
-                <div className='gradient'/>
-            </div>
-
-            <main className='app'>
-                {children}
-            </main>
-        </body>
-
-      
-    </html>
-  )
-}
-
-export default RootLayout
+export default RootLayout;
